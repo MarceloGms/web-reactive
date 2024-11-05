f0c0344b33bfd81a0a4a9a2bb9762ce04c772136
jpedro058 <joao_pedro_12345@hotmail.com>
2024-11-05 18:42:10 +0000

    fix mediaUser service to use reactive

--- src/main/java/com/app/web_reactive/persistence/repository/RelationshipRepository.java
+++ src/main/java/com/app/web_reactive/persistence/repository/RelationshipRepository.java
@@ -4,5 +4,11 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 import com.app.web_reactive.persistence.entity.MediaUsers;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface RelationshipRepository extends ReactiveCrudRepository<MediaUsers, Long> {
+    Flux<MediaUsers> findByUsersIdentifier(long userId);
+
+    Mono<Integer> deleteByMediaIdentifierAndUsersIdentifier(long mediaIdentifier, long usersIdentifier);
 }
